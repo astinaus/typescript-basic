8cc4a09790791b0ab9277348212153c624b74d1a
astinaus <slaayers0@gmail.com>
2023-07-05 13:03:34 +0900

    20230705 타입스크립트 학습

--- todo-project/src/index.ts
+++ todo-project/src/index.ts
@@ -5,6 +5,7 @@ const data = [
   { id: 1, task: "장보기", complete: true },
   { id: 2, task: "TS 공부하기", complete: false },
 ];
+// 선언문과 실행문이 동시에 작성되었기 때문에 타입 추론이 적용되었다.
 
 console.log('My Todo List');
 
